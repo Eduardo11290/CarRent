b107e8ff07269fa899e0eb3cc9794201bb6cc128
Eduardo11290 <petru-eduard.stefoni@student.upt.ro>
2026-01-10 00:43:25 +0200

    config

--- CarRent.Backend/obj/Debug/net10.0/CarRent.Backend.AssemblyInfo.cs
+++ CarRent.Backend/obj/Debug/net10.0/CarRent.Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarRent.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70c02d14320f9e627b1083e2d530c4fd52b62c6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+471b7a93360e2b9cbf0749141196d24c0bd82bfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarRent.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarRent.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
